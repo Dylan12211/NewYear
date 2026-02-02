c0729caf4d2996ebc45060181fbf998d24d0c684
Dylan12211 <149658291+Dylan12211@users.noreply.github.com>
2026-02-02 22:12:44 +0700

    Add GitHub Pages deployment

--- src/logic/config.ts
+++ src/logic/config.ts
@@ -21,15 +21,23 @@ export const photoFiles: string[] = [
   `${BASE}assets/images/image3.jpeg`,
   `${BASE}assets/images/image4.jpeg`,
   `${BASE}assets/images/image5.jpeg`,
+  `${BASE}assets/images/image6.jpeg`,
+  `${BASE}assets/images/image7.jpeg`,
+  `${BASE}assets/images/image8.jpeg`,
+  `${BASE}assets/images/image9.jpeg`,
 ];
 
 
 export const photoMessages: string[] = [
-  "Wishing you joy and happiness",
-  "May your dreams come true",
-  "Season's greetings and best wishes",
-  "Sending love and warm wishes",
-  "Happy holidays and new year",
+  "Chúc Lomiu hay ăn chóng lớn",
+  "Chúc Lomiu lớn đẹp như hoa",
+  "Chúc Lomiu luôn luôn khỏe mạnh",
+  "Chúc Lomiu thúi đừng đánh Cường",
+  "Chúc Lomiu tốt nghiệp thành công",
+  "Chúc Lomiu ngày càng giàu có",
+  "Chúc Lomiu 14/2 vui vẻ",
+  "Chúc Lomiu tiền vào như nước sông đà, tiền ra nhỏ giọt như cà phê phin",
+  "HAPPY NEW YEAR",
 ];
 
 export const state: AppState = {
