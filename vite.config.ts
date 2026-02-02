2bfdeb9aa254abad35f9324373ffeace69e0c156
Dylan12211 <149658291+Dylan12211@users.noreply.github.com>
2026-02-02 19:42:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/merry-christmas/",
+  base: "//NewYear/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
